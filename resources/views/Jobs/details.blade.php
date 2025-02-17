848f12094c21ee78a03855beb4ff31a7f941276e
AbdulsalamHebras <salamhebras@gmail.com>
2025-02-18 00:56:17 +0300

    fix

--- resources/views/Jobs/details.blade.php
+++ resources/views/Jobs/details.blade.php
@@ -1,13 +1,52 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ar" dir="rtl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>details</title>
+    <title> {{$job->jobName}} </title>
+<link rel="stylesheet" href="{{asset('CSS/jobs/details.css')}}">
 </head>
-<body>
-        <p>{{$job->jobName}}</p>
+<body class="job-page">
+    @include('includes.header')
+    <div class="job-header">
+        <h2 class="job-title">{{$job->jobName}}</h2>
+        <div class="company">
+            <img src="{{ asset('storage/logos/' . $job->company->logo) }}" alt="Logo" class="logo">
+            <span>{{ $job->company->name }}</span>
+        </div>
+        <div class="job-details">
+            <p class="location">{{$job->location}}</p>
+            <p class="time">
+                @if($job->updated_at > $job->created_at)
+                    {{ $job->updated_at->locale('ar')->diffForHumans() }}
+                @else
+                    {{ $job->created_at->locale('ar')->diffForHumans() }}
+                @endif
+            </p>
+            <p class="job-type">نوع الوظيفة: {{ $job->jobType }}</p>
+            <p class="salary">الحد الأدنى للراتب: {{ $job->minSalary }} {{$job->currency}} | الحد الأقصى للراتب: {{$job->currency}} {{ $job->maxSalary }} </p>
+        </div>
+        <p>اخر موعد للتقديم: {{$job->expirationDate}} </p>
+        <a href="#" class="apply-btn">التقديم على موقع الشركة</a>
+    </div>
 
+    <div class="content">
+        <div class="notification">
+            <label class="switch">
+                <input type="checkbox">
+                <span class="slider round"></span>
+            </label>
+            <span>أنشئ تنبيهاً وظيفياً للوظائف المشابهة</span>
+        </div>
+        <h3 class="section-title">الوصف الوظيفي</h3>
+        <p class="job-description">
+            {{$job->description}}
+        </p>
+        <h3 class="section-title"> مطلبات الوظيفة</h3>
+        <p class="job-description">
+            {{$job->requirements}}
+        </p>
+    </div>
+    @include('includes.footer')
 </body>
 </html>
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="{{asset('CSS/includes/header.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
-<body>
+<body class="general-page">
     <header>
 
         <div class="header-container">
