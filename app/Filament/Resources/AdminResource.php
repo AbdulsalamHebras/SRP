a0526198eed0f46637b1be0cf564853d3d555611
wesamdreib <wesamdreib2@gmail.com>
2025-04-10 20:54:13 +0300

    v

--- app/Filament/Resources/AdminResource.php
+++ app/Filament/Resources/AdminResource.php
@@ -9,7 +9,7 @@ use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Hash; 
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
